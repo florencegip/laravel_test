7c0dd4550f92176d5abfb65ec81399206e98fba2
florence <florence.puech@gip-retraite.fr>
2017-08-22 15:55:38 +0200

    modification du message d'accueil de la homepage UR

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ Route::get('/', function () {
 });
 
 Route::get('/ur/home', function () {
-    return "Bienvenue sur la home Page UR";
+    return "Bienvenue sur la home Page UR de l'Union Retraite";
 });
